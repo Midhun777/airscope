d9863a86030e6e11fb92a677ac31cc798d2f464c
Midhun Mathew <midmat4828@gmail.com>
2024-12-13 00:17:29 +0530

    bugs and fixes

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+let searchBox = document.getElementById("searchBox");
+let searchButton = document.getElementById("searchButton");
+let cityName = document.getElementById("cityName");
 let weatherValue = document.getElementById("weatherValue");
 let feelsLike = document.getElementById("feelsLike");
 let humidity = document.getElementById("humidity");
@@ -12,34 +15,47 @@ let o3 = document.getElementById("o3");
 let so2 = document.getElementById("so2");
 let nh3 = document.getElementById("nh3");
 let dateDisplay = document.getElementById("date");
+let lat = 0;
+let lon = 0;
+const WEATHERAPI = 'https://api.openweathermap.org/data/2.5/weather?&units=metric&appid=0eb2b840d53255297795ddcca37052e5&q=';
+// const WEATHERAPI = `https://api.openweathermap.org/data/2.5/weather?&units=metric&appid=0eb2b840d53255297795ddcca37052e5&q=${searchBox.value}`;
 
-const WEATHERAPI = "https://api.openweathermap.org/data/2.5/weather?&units=metric&appid=0eb2b840d53255297795ddcca37052e5&q=kochi";
+function getData() {
+    fetch(WEATHERAPI + searchBox.value).then(res => res.json()).then(data => {
+        console.log(data);
+        cityName.innerHTML = data.name;
+        weatherValue.innerHTML = Math.round(data.main.temp) + "°c";
+        feelsLike.innerHTML = Math.round(data.main.feels_like) + "°c";
+        humidity.innerHTML = data.main.humidity + "%";
+        windSpeed.innerHTML = data.wind.speed + "km/h";
+        lat = data.coord.lat;
+        lon = data.coord.lon;
 
-fetch(WEATHERAPI).then(res => res.json()).then(data => {
-    weatherValue.innerHTML = Math.round(data.main.temp) + "°c";
-    feelsLike.innerHTML = Math.round(data.main.feels_like) + "°c";
-    humidity.innerHTML = data.main.humidity + "%";
-    windSpeed.innerHTML = data.wind.speed + "km/h";
-}).catch(e => {
-    console.log(e);
-})
+        return lat, lon;
+    }).catch(e => {
+        console.log(e);
+    })
 
-const AQIURL = "http://api.openweathermap.org/data/2.5/air_pollution?lat=28&lon=77&appid=0fe2bb52b02bfef86aef31e2c09e7e56";
+// console.log(getData())
 
-fetch(AQIURL).then(res => res.json()).then(data => {
-    console.log(data);
-    aqiValue.innerHTML = data.list[0].main.aqi;
-    pmValue1.innerHTML = data.list[0].components.pm2_5;
-    pmValue2.innerHTML = data.list[0].components.pm10;
-    co.innerHTML = data.list[0].components.co;
-    no.innerHTML = data.list[0].components.no;
-    no2.innerHTML = data.list[0].components.no2;
-    o3.innerHTML = data.list[0].components.o3;
-    so2.innerHTML = data.list[0].components.so2;
-    nh3.innerHTML = data.list[0].components.nh3;
-}).catch(e => {
-    console.log(e);
-})
+    const AQIURL = `http://api.openweathermap.org/data/2.5/air_pollution?lat=${lat}&lon=${lon}&appid=0fe2bb52b02bfef86aef31e2c09e7e56`;
+
+    fetch(AQIURL).then(res => res.json()).then(data => {
+        console.log(data);
+        aqiValue.innerHTML = data.list[0].main.aqi;
+        pmValue1.innerHTML = data.list[0].components.pm2_5;
+        pmValue2.innerHTML = data.list[0].components.pm10;
+        co.innerHTML = data.list[0].components.co;
+        no.innerHTML = data.list[0].components.no;
+        no2.innerHTML = data.list[0].components.no2;
+        o3.innerHTML = data.list[0].components.o3;
+        so2.innerHTML = data.list[0].components.so2;
+        nh3.innerHTML = data.list[0].components.nh3;
+    }).catch(e => {
+        console.log(e);
+    })
+
+}
 
 function getFormattedDate(dateInput = new Date()) {
     const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
@@ -47,13 +63,14 @@ function getFormattedDate(dateInput = new Date()) {
         "January", "February", "March", "April", "May", "June",
         "July", "August", "September", "October", "November", "December"
     ];
-
     const day = days[dateInput.getDay()];
     const month = months[dateInput.getMonth()];
     const date = dateInput.getDate();
-
     dateDisplay.innerHTML = day + "," + month + " " + date;
-    console.log(day + "," + month + " " + date)
 }
 
 getFormattedDate();
+
+searchButton.addEventListener("click", function () {
+    getData();
+})
